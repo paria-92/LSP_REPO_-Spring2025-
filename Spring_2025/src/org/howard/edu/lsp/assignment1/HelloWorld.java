4964a98b29d5e913c4fda401a2010669769e82ee
rezae <rezae@10.41.129.56>
2025-01-23 11:34:20 -0500

    First commit.

---
+++ Spring_2025/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+*  Name: Paria Rezaei
+*/
+
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
