b9d1c4d6a7a18e260b5e51c4cd87b6d543c7ac9c
rezae <rezae@10.26.142.131>
2025-01-23 14:39:11 -0500

    Second time.

--- Spring_2025/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ Spring_2025/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -4,7 +4,10 @@
 package org.howard.edu.lsp.assignment1;
 
 public class HelloWorld {
-
+    /**
+     * Paria Rezaei
+     * @param args
+     */
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
 
