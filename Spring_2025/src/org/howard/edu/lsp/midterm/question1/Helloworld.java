12fb4ad244aea48312106381362397e1edf217c1
paria-92 <paria.rezaei@bison.haward.edu>
2025-03-20 14:39:30 -0400

    Added PracticeFile.java for Git testing.

--- Spring_2025/src/org/howard/edu/lsp/midterm/question1/Helloworld.java
+++ Spring_2025/src/org/howard/edu/lsp/midterm/question1/Helloworld.java
@@ -1,6 +1,6 @@
 package org.howard.edu.lsp.midterm.question1;
 
-public class HelloWorld {
+public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Hello, Midterm!");
         System.out.println("This is an updated version of HelloWorld.java.");
