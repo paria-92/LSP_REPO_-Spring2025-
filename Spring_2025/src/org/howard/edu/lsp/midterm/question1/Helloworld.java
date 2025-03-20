8464349952bb80afc004cdc2651487012251cfca
paria-92 <paria.rezaei@bison.haward.edu>
2025-03-20 12:54:31 -0400

    Added HelloWorld.java for midterm preparation

---
+++ Spring_2025/src/org/howard/edu/lsp/midterm/question1/Helloworld.java
@@ -0,0 +1,11 @@
+package org.howard.edu.lsp.midterm.question1;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Midterm!");
+		// TODO Auto-generated method stub
+
+	}
+
+}
