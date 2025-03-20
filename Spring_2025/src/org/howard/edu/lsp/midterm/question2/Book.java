e88e937036d5c884d39b37d15be28a7d943d75cc
paria-92 <paria.rezaei@bison.haward.edu>
2025-03-20 19:01:49 -0400

    Added Book and BookDriver classes for midterm question 2.

---
+++ Spring_2025/src/org/howard/edu/lsp/midterm/question2/Book.java
@@ -0,0 +1,55 @@
+package org.howard.edu.lsp.midterm.question2;
+
+/**
+ * Represents a book in a library.
+ */
+public class Book {
+    private String title;
+    private String author;
+    private String ISBN;
+    private int yearPublished;
+
+    /**
+     * Constructor to initialize a book.
+     */
+    public Book(String title, String author, String ISBN, int yearPublished) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.yearPublished = yearPublished;
+    }
+
+    // Getters and Setters
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+
+    public String getISBN() { return ISBN; }
+    public void setISBN(String ISBN) { this.ISBN = ISBN; }
+
+    public int getYearPublished() { return yearPublished; }
+    public void setYearPublished(int yearPublished) { this.yearPublished = yearPublished; }
+
+    /**
+     * Overrides equals() to compare books by ISBN and author.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true; // Same reference
+        if (obj == null || getClass() != obj.getClass()) return false; // Type check
+
+        Book book = (Book) obj;
+        return this.ISBN.equals(book.ISBN) && this.author.equals(book.author);
+    }
+
+    /**
+     * Overrides toString() to provide a meaningful string representation.
+     */
+    @Override
+    public String toString() {
+        return "Title: " + title + ", Author: " + author + 
+               ", ISBN: " + ISBN + ", Year Published: " + yearPublished;
+    }
+}
---
+++ Spring_2025/src/org/howard/edu/lsp/midterm/question2/BookDriver.java
@@ -0,0 +1,21 @@
+package org.howard.edu.lsp.midterm.question2;
+
+/**
+ * Driver class to test the Book class.
+ */
+public class BookDriver {
+    public static void main(String[] args) {
+        // Creating book objects
+        Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "123456789", 1925);
+        Book book2 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "123456789", 1925);
+        Book book3 = new Book("A Promised Land", "Barack Obama", "9780593239681", 2020);
+
+        // Comparing books using equals() method
+        System.out.println(book1.equals(book2)); // Expected Output: true
+        System.out.println(book1.equals(book3)); // Expected Output: false
+
+        // Printing book information using toString()
+        System.out.println(book1);
+    }
+}
+
