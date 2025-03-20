47a21e6e7d4165c4efe3468374ec3e26a31b2cf4
paria-92 <paria.rezaei@bison.haward.edu>
2025-03-20 14:41:26 -0400

    Added Hellowo.java for Git testing.

--- Spring_2025/src/org/howard/edu/lsp/midterm/question2/Hellowo.java
+++ Spring_2025/src/org/howard/edu/lsp/midterm/question2/Hellowo.java
@@ -1,5 +1,11 @@
 package org.howard.edu.lsp.midterm.question2;
 
 public class Hellowo {
-
+    public static void main(String[] args) {
+        System.out.println("Hello, Midterm!");
+        System.out.println("This is an updated version of HelloWorld.java.");
+        
+        // New Feature: Display a motivational quote
+        System.out.println("Keep pushing forward, you are doing great!");
+    }
 }
