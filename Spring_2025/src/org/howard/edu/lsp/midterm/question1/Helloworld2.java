6738ae085d84e07afec06b2e05958944a8af03f7
paria-92 <paria.rezaei@bison.haward.edu>
2025-03-20 15:52:59 -0400

    Restored HelloWorld2.java after accidental deletion.

---
+++ Spring_2025/src/org/howard/edu/lsp/midterm/question1/Helloworld2.java
@@ -0,0 +1,12 @@
+package org.howard.edu.lsp.midterm.question1;
+
+public class Helloworld2 {
+    public static void main(String[] args) {
+        System.out.println("Hello, Midterm!");
+        System.out.println("This is an updated version of HelloWorld.java.");
+        
+        // New Feature: Display a motivational quote
+        System.out.println("Keep pushing forward, you are doing great!");
+    }
+}
+
