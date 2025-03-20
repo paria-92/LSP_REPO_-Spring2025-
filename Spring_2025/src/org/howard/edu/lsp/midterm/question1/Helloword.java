6d5c37b259e50fdb08f0c112212ed31892c1a4f8
paria-92 <paria.rezaei@bison.haward.edu>
2025-03-20 15:16:44 -0400

    Recreated midterm.question1 and added HelloWorld.java again.

---
+++ Spring_2025/src/org/howard/edu/lsp/midterm/question1/Helloword.java
@@ -0,0 +1,12 @@
+package org.howard.edu.lsp.midterm.question1;
+
+public class Helloword {
+    public static void main(String[] args) {
+        System.out.println("Hello, Midterm!");
+        System.out.println("This is an updated version of HelloWorld.java.");
+        
+        // New Feature: Display a motivational quote
+        System.out.println("Keep pushing forward, you are doing great!");
+    }
+}
+
